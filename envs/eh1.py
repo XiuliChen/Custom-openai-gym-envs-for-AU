fa72fccad58b413a3ada93b4d011cd055745fae0
XiuliChen <Xiuli.chen618@gmail.com>
2020-10-20 12:33:17 +0300

    gaze.py: the gaze module has multiple steps

--- envs/eh1.py
+++ envs/eh1.py
@@ -3,6 +3,7 @@ import numpy as np
 import gym
 from gym import spaces
 import math
+from vel_model import _vel_profile
 
 ###########################################################
 def _calc_dis(p,q):
@@ -17,12 +18,21 @@ class EH1(gym.Env):
 		super(EH1,self).__init__()
 
 		# task setting
+		self.scale_deg=20 # 1.0 in the cavas equals to 20 degress
+
 		self.fitts_W=fitts_W
 		self.fitts_D=fitts_D
 		# agent ocular motor noise and visual spatial noise
 		self.ocular_std=ocular_std
 		self.swapping_std=swapping_std
 		self.motor_std=motor_std
+
+		self.max_steps=10
+
+		self.time_step=int(20) # 50 ms
+		self.time_prep_eye=self.time_step # how many ms
+		self.time_prep_hand=self.time_step*2
+		self.time_fixation=self.time_step*2
 
 		self.action_space = spaces.Box(low=-1, high=1, shape=(4, ), dtype=np.float32)
 
@@ -39,17 +49,22 @@ class EH1(gym.Env):
 		# (2) eye
 		self.pos_eye=np.array([0.0,0.0])
 		self.moving_to_eye=np.array([0.0,0.0])
-		self.stage_eye=0
+		self.prep_eye=0
+		self.fixate_eye=0
+
 		self.vel_eye=0.0
-		self.eye_status=np.concatenate((self.pos_eye,self.moving_to_eye,self.stage_eye,self.vel_eye),axis=None)
+		self.eye_status=np.concatenate((self.pos_eye,self.prep_eye,self.moving_to_eye,self.fixate_eye,self.vel_eye),axis=None)
 
 
 		# (3) hand
 		self.pos_hand=np.array([0.0,0.0])
 		self.moving_to_hand=np.array([0.0,0.0])
-		self.stage_hand=0
+		self.prep_hand=0.0
+
+		self.fixate_hand=0
+
 		self.vel_hand=0.0
-		self.hand_status=np.concatenate((self.pos_hand,self.moving_to_hand,self.stage_hand,self.vel_hand),axis=None)
+		self.hand_status=np.concatenate((self.pos_hand,self.prep_hand,self.moving_to_hand,self.fixate_hand,self.vel_hand),axis=None)
 
 		self.state = np.concatenate((self.target_pos,
                               self.eye_status,
@@ -65,12 +80,37 @@ class EH1(gym.Env):
 		self.belief_full=self.state
 		self.belief_full[0:2]=self.belief
 
+		self.n_steps=0
+
+		self.PREP=False
+		self.START_MOVE=False
+		self.MOV=False
+		self.FIX=False
+		
+
 
 
 		return self.belief_full
 
 	def step(self,action):
-		pass
+		self.n_steps+=1
+
+		self._state_transit(action)
+		
+
+
+		self.belief_full={}
+		reward={}
+		done={}
+		
+
+
+
+		if self.n_steps>self.max_steps:
+			done=True
+
+		info={}
+		return self.belief_full, reward, done, info
 
 	################################
 	def _get_obs(self):
@@ -82,14 +122,114 @@ class EH1(gym.Env):
 
 		return obs,obs_uncertainty
 
+	def _state_transit(self,action):
+
+
+		# execute the action: move the eye and/or move the hand
+		eye_move_amp=_calc_dis(action[0:2],self.moving_to_eye)
+		hand_move_amp=_calc_dis(action[2:4],self.moving_to_hand)
+
+		print(f'eye_move_amp={eye_move_amp}')
+		print(f'hand_move_amp={hand_move_amp}')
+
+
+		# new command
+		if eye_move_amp>0.1:
+			self.moving_to_eye=action[0:2]
+			self.moving_to_hand=action[2:4]
+			print('New eye movement')
+			self.PREP=True
+			self.START_MOVE=False
+			self.MOV=False
+			self.FIX=False
+			
+
+
+		if self.PREP:
+			self.prep_eye+=self.time_step
+			print(f'eye prep: {np.round(self.prep_eye/self.time_prep_eye,2)*100} %')
+			
+			if self.prep_eye==self.time_prep_eye:
+				self.PREP=False
+				self.START_MOVE=True
+
+
+		if self.START_MOVE:
+			self.mov_step=0
+			print('Start Moving')
+
+			# intend pos and actual pos (ocular motor noise)
+			move_dis=_calc_dis(self.pos_eye,action[0:2])
+			ocular_noise=np.random.normal(0, self.ocular_std*move_dis, (2,))
+			end_eye= action[0:2] + ocular_noise
+			self.end_eye=np.clip(end_eye,-1,1)
+
+			amp=_calc_dis(self.pos_eye,end_eye)*self.scale_deg
+
+			stage_e,self.trajectory_e,self.velocity_e=_vel_profile(amp,1,self.time_step)
+
+			self.pos_e=[]
+			for r in (self.trajectory_e/amp):
+				self.pos_e.append(self.pos_eye+r*(end_eye-self.pos_eye))
+
+			self.n_move_steps=len(self.trajectory_e)
+			print(f'n_move_steps={self.n_move_steps}')
+
+			self.START_MOVE=False
+			self.MOV=True
+
+			'''
+			print(n_move_steps)
+			print(f'start={self.pos_eye}')
+			print(f'end={end_eye}')
+			print(pos)
+			'''
+		
+
+		if self.MOV:
+			print(f'mov_step={self.mov_step} ')
+
+			# update vel and pos
+			self.vel_eye=self.velocity_e[self.mov_step]
+			self.pos_eye=self.pos_e[self.mov_step]
+			self.mov_step+=1
+
+			if self.mov_step==self.n_move_steps:
+				self.MOV=False
+				self.FIX=True
+				self.fix_step=0
+
+			
+
+		if self.FIX:
+			self.fixate=self.end_eye
+
+			self.fix_step+=self.time_step
+			print(f'Fixating: {np.round(self.fix_step/self.time_fixation,2)*100} %')
+
+			if self.fix_step==self.time_fixation:
+				self.FIX=False
+				print('Info Extract!')
+				self.obs,self.obs_uncertainty=self._get_obs()
+				# update belief
+
+
+
+
+
+
+
+
 
 if __name__=="__main__":
 	env=EH1()
 	obs=env.reset()
 
-	done =False
-	while not done:
-		action=env.action_space.sample()
-		print(f'action={action}')
-		xxx
+	action=env.action_space.sample()
+	print(f'action={action}')
+	for i in range(10):
+		print('--------------------------------------')
+		observation, reward, done, info = env.step(action)
+		if done:
+			break
 
--- envs/gaze.py
+++ envs/gaze.py
@@ -1,7 +1,9 @@
+
 import numpy as np
 import gym
 from gym import spaces
 import math
+from vel_model import _vel_profile
 
 ###########################################################
 def _calc_dis(p,q):
@@ -12,180 +14,246 @@ def _calc_dis(p,q):
 ###########################################################
 
 
+
 class Gaze(gym.Env):
-  '''
-    Description:
-            The agent moves the eye to the target on the display. 
-            Constraints:
-            (1) visual spatial swapping noise (eccentricity dependent)
-            (2) Ocular motor noise (signal dependent)
-            (3) Ocular shift ('jitter when fixation')
+  def __init__(self,fitts_W = 0.05, fitts_D=0.5, ocular_std=0.1, swapping_std=0.2, motor_std=0.1):
+    super(Gaze,self).__init__()
 
-    States: the target position
-            type: Box(2, )
-            [-1,-1] top-left; 
-            [1,1] bottom-right 
-
-    Actions: the fixation position 
-            type: Box(2, )
-            [-1,-1] top-left; 
-            [1,1] bottom-right 
-
-    Observation: the estimate of where the target is based on one obs
-            type: Box(2, )
-            [-1,-1] top-left; 
-            [1,1] bottom-right 
-
-    Belief: the estimate of where the target is based on all obs
-            type: Box(2, );
-            [-1,-1] top-left; 
-            [1,1] bottom-right 
-
-
-    Reward:
-            Reward of 0 is awarded if the eye reach the target.
-            reward of -1 is awared if not
-
-
-    Episode Termination:
-            the eye reaches the target (within self.fitts_W/2 distance)
-            or reach the maximum steps
-  '''
-
-  metadata = {'render.modes': ['console']}
-
-
-  def __init__(self, fitts_W = 0.2, fitts_D=0.5, ocular_std=0.1, swapping_std=0.1):
-    super(Gaze, self).__init__()
     # task setting
+    self.scale_deg=20 # 1.0 in the cavas equals to 20 degress
+
     self.fitts_W=fitts_W
     self.fitts_D=fitts_D
-
     # agent ocular motor noise and visual spatial noise
     self.ocular_std=ocular_std
     self.swapping_std=swapping_std
+    self.motor_std=motor_std
 
-    self.potential_targets=np.array([[0,-self.fitts_D],[0,self.fitts_D],[self.fitts_D,0],[-self.fitts_D,0]])
+    self.time_step=int(50) # the time step for the controller (unit ms)
+    # timings (unit: self.time_step)
+    self.time_prep_eye=self.time_step*2
+    self.time_prep_hand=self.time_step*2
+    self.time_fixation=self.time_step*2
 
-    # Define action and observation space
-    # They must be gym.spaces objects
-    self.state_space = spaces.Box(low=-1, high=1, shape=(2, ), dtype=np.float64)
-    self.action_space = spaces.Box(low=-1, high=1, shape=(2, ), dtype=np.float64)
-    self.observation_space = spaces.Box(low=-1, high=1, shape=(2, ), dtype=np.float64)
-    self.belief_space = spaces.Box(low=-1, high=1, shape=(2, ), dtype=np.float64)
+    self.max_steps=20
 
-    self.max_fixation=1000
-
-
+    # action space and observation space
+    self.action_space = spaces.Box(low=-1, high=1, shape=(2, ), dtype=np.float32)
 
   def reset(self):
-    """
-    Important: the observation must be a numpy array
-    :return: (np.array) 
-    """
+    # STEP1:  initialize the state
+    # the state of the env includes three elements: target, eye
+    
+    # (1) target
 
-    # choose a random target with distance (self.D) away from the center
-    angle=np.random.uniform(0,math.pi*2)
-    y=math.sin(angle)*self.fitts_D
-    x=math.cos(angle)*self.fitts_D
+    angle=np.random.uniform(0,math.pi*2) 
+    x_target=math.cos(angle)*self.fitts_D
+    y_target=math.sin(angle)*self.fitts_D
+    self.target_pos = np.array([x_target,y_target])
 
-    self.state = np.array([x,y])
 
+    # (2) eye
+    self.pos_eye=np.array([0.0,0.0])
+    self.moving_to_eye=np.array([0.0,0.0])
+    '''
+    self.prep_eye=0
+    self.fixate_eye=0
+
+    self.vel_eye=0.0
+    self.eye_status=np.concatenate((self.pos_eye,self.prep_eye,self.moving_to_eye,self.fixate_eye,self.vel_eye),axis=None)
+
+
+
+    self.state = np.concatenate((self.target_pos,
+                              self.eye_status),axis=None)
+    '''
+
+
+    # step 2: initial obs and belief
+    # first obs and belief
     self.fixate=np.array([0,0])
-    self.n_fixation=1
-
-    # the first obs
     self.obs,self.obs_uncertainty=self._get_obs()
 
-    # the initial belief
     self.belief,self.belief_uncertainty=self.obs, self.obs_uncertainty
+    
+    '''
+    self.belief_full=self.state
+    self.belief_full[0:2]=self.belief
+    '''
+
+    print(f'target_pos={self.target_pos}')
+    print(f'inital belief={self.belief}')
+
+    self.n_steps=0
+
+    self.PREP=False
+    self.START_MOVE=False
+    self.MOV=False
+    self.FIX=False
+
+    
+
+
+
     return self.belief
 
-
-  def step(self, action):
-
-    # execute the chosen action (Ocular motor noise, signal dependent)
-    move_dis=_calc_dis(self.fixate,action)
-    ocular_noise=np.random.normal(0, self.ocular_std*move_dis, action.shape)
-    self.fixate= action + ocular_noise
-    self.fixate=np.clip(self.fixate,-1,1)
-    self.n_fixation+=1
+  def step(self,action):
+    self.n_steps+=1
+    self._state_transit(action)
 
     # check if the eye is within the target region
-    dis_to_target=_calc_dis(self.state, self.fixate)
+    dis_to_target=_calc_dis(self.target_pos, self.fixate)
+
     if  dis_to_target < self.fitts_W/2:
       done = True
       reward = 0
     else:
       done = False
       reward = -1 
-      # has not reached the target, get new obs at the new fixation location
-      self.obs,self.obs_uncertainty=self._get_obs()
-      self.belief,self.belief_uncertainty=self._get_belief()
-
-    if self.n_fixation>self.max_fixation:
-        done=True
 
 
-    info={'target': self.state, 
-      'belief': self.belief,
-      'aim': action,
-      'fixate': self.fixate,
-      'fitts_W':self.fitts_W,
-      'fitts_D':self.fitts_D}
-    # Optionally we can pass additional info, we are not using that for now
+    if self.n_steps>self.max_steps:
+      done=True
+
+    info={}
     return self.belief, reward, done, info
 
-    
+ 
 
-    #return np.array([self.agent_pos]).astype(np.float32), reward, done, info
-
-  def render(self, mode='console'):
-    if mode != 'console':
-      raise NotImplementedError()
-    pass
-
-  def close(self):
-    pass
-
+  ################################
   def _get_obs(self):
-    eccentricity=_calc_dis(self.state,self.fixate)
+    eccentricity=_calc_dis(self.target_pos,self.fixate)
     obs_uncertainty=eccentricity
-    spatial_noise=np.random.normal(0, self.swapping_std*eccentricity, self.state.shape)
-    obs=self.state + spatial_noise
+    spatial_noise=np.random.normal(0, self.swapping_std*eccentricity, self.target_pos.shape)
+    obs=self.target_pos + spatial_noise
     obs=np.clip(obs,-1,1)
 
     return obs,obs_uncertainty
 
+
   def _get_belief(self):
     z1,sigma1=self.obs,self.obs_uncertainty
     z2,sigma2=self.belief,self.belief_uncertainty
+
     w1=sigma2**2/(sigma1**2+sigma2**2)
     w2=sigma1**2/(sigma1**2+sigma2**2)
+
     belief=w1*z1+w2*z2
     belief_uncertainty=np.sqrt( (sigma1**2+sigma2**2)/(sigma1**2 * sigma2**2))
 
     return belief, belief_uncertainty
 
+  def _state_transit(self,action):
+    # execute the action: move the eye and/or move the hand
+    eye_move_amp=_calc_dis(action[0:2],self.moving_to_eye)
+    #hand_move_amp=_calc_dis(action[2:4],self.moving_to_hand)
+
+    
+    if eye_move_amp>0.005:
+      # new eye command
+      self.moving_to_eye=action[0:2]
+      print(f'New eye target:{action[0:2]}')
+      self.PREP=True
+      self.prep_eye=0
+      self.START_MOVE=False
+      self.MOV=False
+      self.FIX=False
+
+    if self.PREP:
+      print(f'eye prep: {self.prep_eye} ms')
+      prep_stage=np.round(self.prep_eye/self.time_prep_eye,2)
+      
+
+      self.prep_eye+=self.time_step
+      if self.prep_eye>self.time_prep_eye:
+        self.PREP=False
+        self.START_MOVE=True
+
+
+    if self.START_MOVE:
+      # intend pos and actual pos (ocular motor noise)
+      move_dis=_calc_dis(self.pos_eye,action[0:2])
+      ocular_noise=np.random.normal(0, self.ocular_std*move_dis, (2,))
+      end_eye= action[0:2] + ocular_noise
+      self.end_eye=np.clip(end_eye,-1,1)
+      amp=_calc_dis(self.pos_eye,end_eye)*self.scale_deg
+      stage_e,self.trajectory_e,self.velocity_e=_vel_profile(amp,1,self.time_step)
+      self.pos_e=[]
+      for r in (self.trajectory_e/amp):
+        self.pos_e.append(self.pos_eye+r*(end_eye-self.pos_eye))
+      
+      self.n_move_steps=len(self.trajectory_e)
+      print(f'Start Moving to {self.end_eye}')
+
+
+
+      
+      self.mov_step=0
+      self.START_MOVE=False
+      self.MOV=True
+
+      '''
+      print(n_move_steps)
+      print(f'start={self.pos_eye}')
+      print(f'end={end_eye}')
+      print(pos)
+      '''
+    
+
+    if self.MOV:
+      mov_stage=np.round(self.mov_step/(self.n_move_steps),2)
+
+      # update vel and pos
+      self.vel_eye=self.velocity_e[self.mov_step]
+      self.pos_eye=self.pos_e[self.mov_step]
+
+
+      self.mov_step+=1
+      if self.mov_step==self.n_move_steps:
+        self.MOV=False
+        self.FIX=True
+        self.fix_step=0
+        
+
+      
+
+    if self.FIX:
+      print(f'Fixating: {self.fix_step} ms')
+      self.fix_step+=self.time_step
+
+      if self.fix_step>self.time_fixation:
+        print('Info Extract!')
+        self.fixate=self.end_eye
+        self.FIX=False
+        self.obs,self.obs_uncertainty=self._get_obs()
+        self.belief,self.belief_uncertainty=self._get_belief()
+        print(f'self.belief={self.belief}')
+        # update belief
+
+
+
+
+
+
 
 
 
 if __name__=="__main__":
-  env = Gaze()
+  env=Gaze()
   obs=env.reset()
-  print('init obs=', obs)
-  done =False
-  while not done:
-    action = env.action_space.sample() # your agent here (this takes random actions)
-    print('action=', np.round(action,2))
+  action=env.action_space.sample()
 
+  for i in range(20):
+    print('--------------------------------------')
+    print(f'step={i}, time:{(i-1)*50}ms--{(i)*50}ms')
     observation, reward, done, info = env.step(action)
-    print('belief=', np.round(observation,2), 'reward=', reward, 'done=', done)
+    action=observation
+    print(f'observation, reward, done={observation}, {reward}, {done}')
+    print(f'eye pos={env.pos_eye}')
 
 
-  env.close()
 
 
-
-
+    if done:
+      break
 
